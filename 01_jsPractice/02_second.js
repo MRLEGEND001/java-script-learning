ae9cec4f842b2c7b92cafb3becc8eb673a46c06e
MRLEGEND001 <152675286+MRLEGEND001@users.noreply.github.com>
2024-01-08 03:24:35 +0000

    config done

--- 01_jsPractice/02_second.js
+++ 01_jsPractice/02_second.js
@@ -1,10 +1,24 @@
 const number_1 = 12 ;
 const number_2 = 12 ;
 
-console.log(number_1 + number_2 );
-console.log(typeof(23));
-console.log(typeof("mrlegend"));
-console.log(typeof(true));
-console.log(typeof(''));
-console.log(typeof(null));
-console.log(typeof({}));
+// console.log(number_1 + number_2 );
+// console.log(typeof(23));
+// console.log(typeof("mrlegend"));
+// console.log(typeof(true));
+// console.log(typeof(''));
+// console.log(typeof(null));
+// console.log(typeof({}));
+
+//variable let , var , const //
+//prefer is not use to var
+//login user email and password think that usualy//
+const accoutid = "000001";  //accoutid = 02 not allo another const variable//
+const accoutgmail = "legned@2gamil.com";
+const accountusername = "mrlegned001@gmail.com";
+var accoutpassword = "121212@";
+let accoutncity = "Korba";
+placeholder = "legend"; //possibility but that shoud be not use
+let cityname;
+
+console.log([accoutid,accoutgmail,accoutpassword,accoutncity]);
+console.table([accoutid,accoutgmail,accoutpassword,accoutncity,placeholder,cityname]);
